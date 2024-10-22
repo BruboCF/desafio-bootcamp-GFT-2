aef373dc83e325fd6960a6c42d3db4f4310870e4
Bruno Fortunatti <bruno.fortunatti4170@gmail.com>
2024-10-22 15:41:32 -0300

    index.js done

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-let vitorias = 58;
+let vitorias = 200;
 let derrotas = 11;
 let patentes = ["Ferro", "Bronze", "Prata", "Ouro", "Diamante", "Lendário", "Imortal"];
-let requisito = [10, 20, 50, 80, 90, 100, 101];
+let requisito = [10, 20, 50, 80, 90, 100];
 
 function saldo(v, d) {
     return v - d;
 }
 
-let saldoVitorias = rank(vitorias, derrotas);
+let saldoVitorias = saldo(vitorias, derrotas);
 
 function rank(saldoVitorias) {
-    let nivel;
-    
+    let nivel, i;
+
+    for(i = 0; i < 6; i++) {
+        if(saldoVitorias < requisito[i]) {
+            nivel = patentes[i];
+            break;
+        }
+    }
+    //Caso o valor seja "Imortal" será implementado aqui
+    nivel = patentes[i];
+
+    return nivel;
 }
 
-let rank = rank(saldoVitorias);
+let nivelFinal = rank(saldoVitorias);
+
+console.log(`O Herói tem de saldo ${saldoVitorias} e está no nível de ${nivelFinal}`);
 
 
 
